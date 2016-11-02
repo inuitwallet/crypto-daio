54679947e926fcab03f86c86aeabed0ae3ea0dae
sammoth <sam.griffiths@wazoku.com>
2016-11-02 11:22:21 +0000

    update admin page layout

--- blocks/admin.py
+++ blocks/admin.py
@@ -13,7 +13,27 @@ class TransactionAdmin(admin.ModelAdmin):
     list_display = ('tx_id',)
 
 admin.site.register(Transaction, TransactionAdmin)
-admin.site.register(TxInput)
-admin.site.register(TxOutput)
-admin.site.register(Address)
-admin.site.register(WatchAddress)
+
+
+class TxInputAdmin(admin.ModelAdmin):
+    list_display = ('transaction', 'tx_id', 'vout')
+
+admin.site.register(TxInput, TxInputAdmin)
+
+
+class TxOutputAdmin(admin.ModelAdmin):
+    list_display = ('transaction', 'n', 'value')
+
+admin.site.register(TxOutput, TxOutputAdmin)
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('address')
+
+admin.site.register(Address, AddressAdmin)
+
+
+class WatchAddressAdmin(admin.ModelAdmin):
+    list_display = ('address', 'amount', 'callback', 'complete')
+
+admin.site.register(WatchAddress, WatchAddressAdmin)
