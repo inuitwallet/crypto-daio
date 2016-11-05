2e2a59056acd91193b7394c3195c4e466b45b1a3
sammoth <sam.griffiths@wazoku.com>
2016-11-05 15:07:50 +0000

    adjust notify urls

--- blocks/urls.py
+++ blocks/urls.py
@@ -5,12 +5,20 @@ base64_pattern = r'(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)
 
 urlpatterns = [
     url(
-        r'^notify/(?P<block_hash>{})'.format(base64_pattern),
+        r'^notify/block/(?P<block_hash>{})'.format(base64_pattern),
         views.notify,
         name='notify'
     ),
     url(r'^parse/', views.parse, name='parse'),
     url(r'^blocks/$', views.BlockList.as_view(), name='all_blocks'),
-    url(r'^block/(?P<block_hash>\w+)', views.BlockDetail.as_view(), name='get_block'),
-    url(r'^tx/(?P<tx_id>\w+)', views.TransactionDetail.as_view(), name='get_tx'),
+    url(
+        r'^block/(?P<block_hash>{})'.format(base64_pattern),
+        views.BlockDetail.as_view(),
+        name='get_block'
+    ),
+    url(
+        r'^tx/(?P<tx_id>{})'.format(base64_pattern),
+        views.TransactionDetail.as_view(),
+        name='get_tx'
+    ),
 ]
