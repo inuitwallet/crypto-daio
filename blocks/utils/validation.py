b781a542b2763dea3a5914cfe3d1e1c6ab39ec79
sammoth <sam.griffiths@wazoku.com>
2016-11-29 00:03:37 +0000

    tidy up logging

--- blocks/utils/validation.py
+++ blocks/utils/validation.py
@@ -86,10 +86,12 @@ if __name__ == '__main__':
         try:
             prev_block_hash = block.previous_block.hash
         except AttributeError:
-            print('couldn\'t get previous block hash for block {}:{}'.format(
-                block.height,
-                i
-            ))
+            print(
+                'couldn\'t get previous block hash for block at height {} (id {})'.format(
+                    block.height,
+                    i
+                )
+            )
             rpc = send_rpc(
                 {
                     'method': 'getblock',
