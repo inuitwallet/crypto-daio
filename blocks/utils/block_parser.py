75696782cc5e07a00703037bc1d53cf265825b38
sammoth <sam.griffiths@wazoku.com>
2016-11-30 10:14:12 +0000

    update block parser to handle missing previous and next blocks more betterer

--- blocks/utils/block_parser.py
+++ blocks/utils/block_parser.py
@@ -20,8 +20,7 @@ tz = timezone.get_current_timezone()
 
 def save_block(block):
     """
-    :param block:
-    :return:
+    Parse and save a Block object.
     """
     # Try and get the bloch hash from the passed block
     try:
@@ -31,8 +30,10 @@ def save_block(block):
         return
     # get or create the block
     this_block, _ = Block.objects.get_or_create(hash=block_hash)
+    # get the height and log the block
     this_block.height = block.get('height', None)
     logger.info('parsing {} at height {}'.format(block_hash, this_block.height))
+    # parse the json and apply to the block we just fetched
     this_block.size = block.get('size', None)
     this_block.version = block.get('version', None)
     this_block.merkle_root = block.get('merkleroot', None)
@@ -46,26 +47,6 @@ def save_block(block):
     this_block.bits = block.get('bits', None)
     this_block.difficulty = block.get('difficulty', None)
     this_block.mint = block.get('mint', None)
-
-    try:
-        previous_block = Block.objects.get(
-            hash=block.get('previousblockhash', None)
-        )
-        this_block.previous_block = previous_block
-        # update the previous block with this block as its next block
-        previous_block.next_block = this_block
-        previous_block.save()
-    except Block.DoesNotExist:
-        pass
-
-    try:
-        next_block = Block.objects.get(
-            hash=block.get('nextblockhash', None)
-        )
-        this_block.next_block = next_block
-    except Block.DoesNotExist:
-        pass
-
     this_block.flags = block.get('flags', None)
     this_block.proof_hash = block.get('proofhash', None)
     this_block.entropy_bit = block.get('entropybit', None)
@@ -73,6 +54,52 @@ def save_block(block):
     this_block.modifier_checksum = block.get('modifierchecksum', None)
     this_block.coinage_destroyed = block.get('coinagedestroyed', None)
 
+    # using the previousblockhash, get the block object to connect
+    prev_block_hash = block.get('previousblockhash', None)
+    try:
+
+        previous_block = Block.objects.get(
+            hash=prev_block_hash
+        )
+        this_block.previous_block = previous_block
+        # update the previous block with this block as its next block
+        previous_block.next_block = this_block
+        previous_block.save()
+    except Block.DoesNotExist:
+        logger.warning('Previous Block {} Does not exist'.format(prev_block_hash))
+        # if the previous block should exist we create it to link it to this block
+        # and then trigger a parse of that block
+        if prev_block_hash is not None:
+            previous_block = Block.objects.create(
+                hash=prev_block_hash,
+                next_block=this_block,
+            )
+            previous_block.save()
+            trigger_block_parse(prev_block_hash)
+
+    # do the same for the next block
+    next_block_hash = block.get('nextblockhash', None)
+    try:
+        next_block = Block.objects.get(
+            hash=next_block_hash
+        )
+        this_block.next_block = next_block
+    except Block.DoesNotExist:
+        logger.warning('Next Block {} Does not exist'.format(next_block_hash))
+        # if the next block should exist we create it to link it to this block
+        # and then trigger a parse of that block
+        if next_block_hash is not None:
+            next_block = Block.objects.create(
+                hash=next_block_hash,
+            )
+            next_block.save()
+            this_block.next_block = next_block
+            trigger_block_parse(next_block.hash)
+
+    # attempt to save this block. Orphan blocks can appear which ruins data integrity
+    # if we find a previous block with the same height already exists we check the hashes
+    # of both. If they differ we prefer the newer block as we casn assume it has come
+    # directly from, the coin daemon.
     try:
         this_block.save()
         logger.info('saved block {}'.format(this_block.height))
@@ -88,29 +115,16 @@ def save_block(block):
                 'hashes match. leaving existing block {}'.format(existing_block.height)
             )
 
+    # ow get the transaction hashes and request their data from the daemon
     for tx_hash in block.get('tx', []):
         logger.info('scanning tx {}'.format(tx_hash))
-        rpc = send_rpc(
-            {
-                'method': 'getrawtransaction',
-                'params': [tx_hash, 1]
-            }
-        )
-        tx = rpc['result'] if not rpc['error'] else None
-        if tx:
-            save = Thread(
-                target=save_transaction,
-                kwargs={
-                    'block': this_block,
-                    'tx': tx,
-                },
-                name=tx_hash
-            )
-            save.daemon = True
-            save.start()
+        trigger_transaction_parse(this_block, tx_hash)
 
 
 def save_transaction(block, tx):
+    """
+    Parse and save a transaction Object
+    """
     # get the transaction details
     transaction, _ = Transaction.objects.get_or_create(
         block=block,
@@ -183,10 +197,8 @@ def save_transaction(block, tx):
 
 def check_watch_addresses(address, value):
     """
-    We check the list of watched addresses and reduce the expected amount by the value
-    :param address:
-    :param value:
-    :return:
+    Check the list of watched addresses and reduce the expected amount by the value.
+    Notify a callback url if one is preset and the value <= 0
     """
     # we work with unique addresses so get() should be safe here
     try:
@@ -210,6 +222,9 @@ def check_watch_addresses(address, value):
 
 
 def start_parse():
+    """
+    Parse the block
+    """
     # Get current height from Coin Daemon
     rpc = send_rpc(
         {
@@ -246,6 +261,11 @@ def check_transaction():
 
 
 def trigger_block_parse(block_hash, blocking=False):
+    """
+    For the given block hash, request the block json from the coin daemon and
+    trigger the save method. specify blocking=True to call the save method directly
+    otherwise it is called in a new thread
+    """
     rpc = send_rpc(
         {
             'method': 'getblock',
@@ -263,6 +283,37 @@ def trigger_block_parse(block_hash, blocking=False):
                     'block': got_block,
                 },
                 name=block_hash
+            )
+            save.daemon = True
+            save.start()
+
+
+def trigger_transaction_parse(block, tx_hash, blocking=False):
+    """
+    For the given block object and transaction hash,
+    request the tx json from the coin daemon and
+    trigger the save method.
+    specify blocking=True to call the save method directly
+    otherwise it is called in a new thread
+    """
+    rpc = send_rpc(
+        {
+            'method': 'getrawtransaction',
+            'params': [tx_hash, 1]
+        }
+    )
+    tx = rpc['result'] if not rpc['error'] else None
+    if tx:
+        if blocking:
+            save_transaction(block, tx)
+        else:
+            save = Thread(
+                target=save_transaction,
+                kwargs={
+                    'block': block,
+                    'tx': tx,
+                },
+                name=tx_hash
             )
             save.daemon = True
             save.start()
