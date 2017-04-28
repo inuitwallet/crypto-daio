332fbb13d87b7ef6c854d22aa47650a40f24494a
sammoth <sam.griffiths@wazoku.com>
2017-04-28 10:46:27 +0100

    move validation and repair functions to channels consumers

---
+++ blocks/consumers/block.py
@@ -0,0 +1,199 @@
+import logging
+
+from channels import Channel
+
+from blocks.models import Block
+from blocks.utils.rpc import send_rpc
+
+logger = logging.getLogger('daio')
+
+
+def parse_block(message):
+    """
+    Process a block. 
+    Create and parse if block didn't previously exist.
+    Fetch and validate if block did previously exist.
+    
+    :param message: asgi valid message containing
+        block_hash (required) string - hash of the block to process
+        parse_next (optional) boolean - set True to move to the next 
+                                        block after passing validation
+    
+    :return boolean
+    """
+    parse_next = message.get('parse_next', False)
+    block_hash = message.get('block_hash')
+    if not block_hash:
+        logger.error('no block hash in message')
+        return False
+
+    logger.info('parsing block {}'.format(block_hash))
+    block, created = Block.objects.get_or_create(
+        hash=block_hash
+    )
+    if created:
+        logger.info('new block created for {}'.format(block.hash))
+        # fetch the rpc data for the block
+        rpc = send_rpc(
+            {
+                'method': 'getblock',
+                'params': [block_hash]
+            }
+        )
+        if rpc['error']:
+            logger.error('rpc error: {}'.format(rpc['error']))
+            return False
+        # parse the block to save it
+        block.parse_rpc_block(rpc['result'])
+        logger.info('saved block {}'.format(block.height))
+
+    else:
+        logger.info('existing block found at {}. validating'.format(block.height))
+        Channel('validate_block').send(
+            {'block_hash': block.hash, 'parse_next': parse_next}
+        )
+
+
+def validate_block(message):
+    """
+    Validate an existing block.
+    If validation passes, validate the blocks transactions and optionally 
+    process the next block.
+    If validation fails, act on the block accordingly
+    
+    Block will have repairs made in it fails validation. 
+    This does not guarantee that the block will pass validation afterwards
+    
+    :param message: asgi valid message containing:
+        block_hash (required) string - hash of block to validate
+        parse_next (optional) boolean - set True to move to the next block 
+                                        after passing validation
+    
+    :return: boolean 
+    """
+    parse_next = message.get('parse_next', False)
+    block_hash = message.get('block_hash')
+
+    if not block_hash:
+        logger.error('no block hash in message')
+        return False
+
+    try:
+        block = Block.objects.get(hash=block_hash)
+    except Block.DoesNotExist:
+        logger.error('no block found with hash {}'.format(block_hash))
+        return False
+
+    valid, error_message = block.validate()
+
+    if valid:
+        logger.info(
+            'block {} at {} is valid. checking transactions'.format(
+                block.hash,
+                block.height
+            )
+        )
+        Channel('validate_transactions').send({'block_hash': block.hash})
+        if parse_next:
+            if not block.next_block:
+                logger.warning('no next block {} found to parse'.format(block.height + 1))
+                return
+            if not block.next_block.hash:
+                logger.error('next block {} has no hash'.format(block.height + 1))
+                return
+            Channel('parse_block').send({'block_hash': block.next_block.hash})
+    else:
+        logger.warning(
+            'INVALID BLOCK {} at {}! {}'.format(block.hash, block.height, error_message)
+        )
+        Channel('repair_block').send(
+            {
+                'block_hash': block.hash,
+                'error_message': error_message,
+                'parse_next': parse_next
+            }
+        )
+
+
+def repair_block(message):
+    """
+    Repair an existing block
+    
+    :param message: asgi valid message containing:
+        block_hash (required) string - hash of block to repair
+        error_message (required) string - validation error message
+        parse_next (optional) boolean - set True to move to the next block 
+                                        after passing validation
+        
+    :return: boolean 
+    """
+    parse_next = message.get('parse_next', False)
+    error_message = message.get('error_message')
+
+    if not error_message:
+        logger.error('no error message passed to repair')
+        return False
+
+    block_hash = message.get('block')
+
+    if not block_hash:
+        logger.error('no block in message')
+        return False
+
+    try:
+        block = Block.objects.get(hash=block_hash)
+    except Block.DoesNotExist:
+        logger.error('no block found with hash {}'.format(block_hash))
+        return False
+
+    # merkle root error means missing, extra or duplicate transactions
+    if message == 'merkle root incorrect':
+        rpc = send_rpc(
+            {
+                'method': 'getblock',
+                'params': [block.hash]
+            }
+        )
+
+        if rpc['error']:
+            logger.error('rpc error: {}'.format(rpc['error']))
+            return False
+
+        transactions = rpc['result'].get('tx', [])
+        block_tx = block.transactions.all().values_list('tx_id', flat=True)
+
+        # add missing transactions
+        for tx in list(set(transactions) - set(block_tx)):
+            logger.info('adding missing tx {}'.format(tx))
+            Channel('parse_transaction').send(
+                {
+                    'tx_hash': tx,
+                    'tx_index': transactions.index(tx),
+                    'block_hash': block.hash
+                }
+            )
+
+        # remove additional transactions
+        for tx in block.transactions.all():
+            if tx.tx_id not in transactions:
+                logger.error(
+                    'tx {} does not belong to block {}'.format(
+                        tx.tx_id,
+                        block.height
+                    )
+                )
+                tx.delete()
+
+        # check for duplicate blocks (shouldn't happen as tx_id id unique)
+        if len(list(set(block_tx))) != len(block_tx):
+            logger.error('detected duplicate transaction')
+            block.transactions.all().delete()
+
+        # validate the transactions
+        Channel('validate_transactions').send({'block': block})
+
+    else:
+        # all other errors with the block can be solved by re-parsing it
+        Channel('parse_block').send(
+            {'block_hash': block.hash, 'parse_next': parse_next}
+        )
---
+++ blocks/consumers/transaction.py
@@ -0,0 +1,171 @@
+import logging
+
+from channels import Channel
+
+from blocks.models import Transaction, Block
+from blocks.utils.rpc import send_rpc
+
+logger = logging.getLogger('daio')
+
+
+def parse_transaction(message):
+    """
+    Process a transaction. 
+    Create and parse if the transaction is new
+    Validate if Transaction already exists
+    If validation fails, re-parse Transaction
+    
+    :param message: asgi valid message containing:
+        tx_hash (required) string - tx_id of transaction rto process
+        block_hash (optional) string - hash of block that transaction belongs to.
+                                       if not passed an rpc call is made to get it.
+        tx_index (optional) string - index of transaction in relation to block.
+                                     if not passed an rpc call is made to get it.
+                                     
+    :return: boolean
+    """
+    rpc = None
+    # get the transaction hash from the message and return if none found
+    tx_hash = message.get('tx_hash')
+
+    if not tx_hash:
+        logger.error('no transaction hash in message')
+        return False
+
+    # get the block from the message
+    block_hash = message.get('block_hash')
+
+    if not block_hash:
+        logger.warning('no block id in message for tx {}'.format(tx_hash))
+        # we are scanning just the transaction so fetch the raw data
+        rpc = send_rpc(
+            {
+                'method': 'getrawtransaction',
+                'params': [tx_hash, 1]
+            }
+        )
+        if rpc['error']:
+            logger.error('rpc error at tx {}: {}'.format(tx_hash, rpc['error']))
+            return False
+
+        # get the block_hash from the transaction
+        block_hash = rpc['result'].get('blockhash')
+
+        if not block_hash:
+            logger.error('no block hash found from tx rpc')
+            return False
+
+    # get the index from the message
+    tx_index = message.get('tx_index')
+
+    if tx_index is None:
+        logger.warning('no tx index in message for tx {}'.format(tx_hash))
+        block_rpc = send_rpc(
+            {
+                'method': 'getblock',
+                'params': [block_hash]
+            }
+        )
+
+        if block_rpc['error']:
+            logger.error(
+                'get tx_index rpc error at tx {} for {} {}'.format(
+                    tx_hash,
+                    block_hash,
+                    block_rpc['error']
+                )
+            )
+            return False
+
+        try:
+            tx_index = block_rpc['result'].get('tx', []).index(tx_hash)
+        except ValueError:
+            logger.error(
+                'transaction doesn\'t belong to block: {} not in {}'.format(
+                    tx_hash,
+                    block_rpc['result'].get('tx', [])
+                )
+            )
+            try:
+                Transaction.objects.get(tx_id=tx_hash).delete()
+                return False
+            except Transaction.DoesNotExist:
+                return False
+
+    block, created = Block.objects.get_or_create(hash=block_hash)
+    if created:
+        logger.warning('block is newly created so parsing full block')
+        Channel('parse_block').send({'block_hash': block_hash})
+        return False
+
+    logger.info('parsing tx {} for block {}'.format(tx_hash, block.height))
+    tx, created = Transaction.objects.get_or_create(
+        tx_id=tx_hash,
+        block=block,
+        index=tx_index
+    )
+    if created:
+        logger.info('new tx {} at block {}'.format(tx_hash, block.height))
+        # fetch the rpc data for the block if we haven't already
+        if not rpc:
+            rpc = send_rpc(
+                {
+                    'method': 'getrawtransaction',
+                    'params': [tx_hash, 1]
+                }
+            )
+            if rpc['error']:
+                logger.error('rpc error: {}'.format(rpc['error']))
+                return
+        # parse the block to save it
+        tx.parse_rpc_tx(rpc['result'])
+    else:
+        logger.info('existing tx {} found at {}'.format(tx_hash, block.height))
+        # validate the transaction
+        valid, error_message = tx.validate()
+        if not valid:
+            # transaction is invalid so re-fetch from rpc and save again
+            logger.warning(
+                'INVALID TX {} at {}! {}'.format(tx_hash, block.height, error_message)
+            )
+            # fetch the rpc data for the block if we haven't already
+            if not rpc:
+                rpc = send_rpc(
+                    {
+                        'method': 'getrawtransaction',
+                        'params': [tx_hash, 1]
+                    }
+                )
+                if rpc['error']:
+                    logger.error('rpc error: {}'.format(rpc['error']))
+                    return
+            # re-parse the block to save it
+            tx.parse_rpc_tx(rpc['result'])
+        else:
+            logger.info('tx {} is valid'.format(tx_hash))
+
+
+def validate_transactions(message):
+    """
+    given a block, validate all transactions
+    :param message: asgi valid message containing:
+        block_hash (required) string - hash of block to use for validations
+    :return: 
+    """
+    block_hash = message.get('block_hash')
+
+    if not block_hash:
+        logger.error('no block in message')
+        return False
+
+    try:
+        block = Block.objects.get(hash=block_hash)
+    except Block.DoesNotExist:
+        logger.error('no block found with hash {}'.format(block_hash))
+        return False
+
+    for tx in block.transactions.all():
+        if not tx.is_valid:
+            Channel('parse_transaction').send(
+                {'tx_hash': tx.tx_id, 'block_hash': block.hash}
+            )
