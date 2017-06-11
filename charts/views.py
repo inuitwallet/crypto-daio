6e54fe44d5ce23bb05edee03e4097edc11702738
sammoth <sam.griffiths@wazoku.com>
2017-06-12 00:21:39 +0100

    udate graph

--- charts/views.py
+++ charts/views.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from django.db import connection
@@ -24,24 +25,49 @@ class CirculatingChart(View):
                 unit=coin.unit_code
             ).order_by(
                 'time_added'
-            )[2628000]
+            )
+
+            x_data = []
+            y_data = []
+            y_value = 0.0
+
+            for info_object in info_objects:
+                day = int(
+                    time.mktime(
+                        datetime.datetime(
+                            info_object.time_added.year,
+                            info_object.time_added.month,
+                            info_object.time_added.day,
+                            0,
+                            0,
+                            0
+                        ).timetuple()
+                    ) * 1000
+                )
+
+                if len(x_data) == 0:
+                    x_data.append(day)
+
+                if day not in x_data:
+                    y_data.append(y_value)
+                    x_data.append(day)
+                    y_value = 0.0
+
+                y_value += float(info_object.money_supply)
+
+            y_data.append(y_value)
+
             coin_data['coins'].append(
                 {
-                    'type': "lineWithFocusChart",
+                    'type': "lineChart",
                     'name': 'Circulating {}'.format(coin.name),
                     'extra': {
                         'x_is_date': True,
                     },
                     'container': '{}_container'.format(coin.unit_code),
                     'data': {
-                        'x': [
-                            int(time.mktime(info_object.time_added.timetuple()) * 1000)
-                            for info_object in info_objects
-                        ],
-                        'y1': [
-                            float(info_object.money_supply)
-                            for info_object in info_objects
-                        ],
+                        'x': x_data,
+                        'y1': y_data,
                         'name1': 'Circulating {}'.format(coin.name),
                         'extra1': extra_data,
                         'color_category': 'category10'
