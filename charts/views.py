0c493bf529761773e7e6b1b8e87b215aa8c146c0
sammoth <sam.griffiths@wazoku.com>
2017-06-12 22:58:44 +0100

    update all series onto one chart

--- charts/views.py
+++ charts/views.py
@@ -2,7 +2,9 @@ import datetime
 import time
 
 from django.db import connection
+from django.db.models import Min, Max, Avg
 from django.shortcuts import render
+from django.utils.timezone import make_aware
 from django.views import View
 
 from blocks.models import Info
@@ -11,72 +13,75 @@ from blocks.models import Info
 class CirculatingChart(View):
     @staticmethod
     def get(request):
-        coin_data = {'coins': []}
-        extra_data = {
-            "tooltip": {
-                "y_start": "There are ",
-                "y_end": " circulating"
+        x_data = []
+        y_data = {}
+
+        low_date = Info.objects.all().aggregate(
+            Min('time_added')
+        )
+        low_date = low_date['time_added__min']
+        max_date = Info.objects.all().aggregate(
+            Max('time_added')
+        )
+        max_date = max_date['time_added__max']
+
+        coins = list(connection.tenant.coins.all())
+
+        for coin in coins:
+            y_data[coin.unit_code] = []
+
+        while low_date < max_date:
+            midnight = make_aware(
+                datetime.datetime(
+                    low_date.year,
+                    low_date.month,
+                    low_date.day,
+                    0,
+                    0,
+                    0
+                )
+            )
+            tomorrow = midnight + datetime.timedelta(days=1)
+            day = int(time.mktime(tomorrow.timetuple()) * 1000)
+            x_data.append(day)
+
+            for coin in coins:
+                info = Info.objects.filter(
+                    unit=coin.unit_code
+                ).aggregate(
+                    Avg('money_supply')
+                )
+                y_data[coin.unit_code].append(info['money_supply__avg'])
+
+            low_date = tomorrow
+
+        series_data = {'x': x_data}
+        index = 1
+
+        for coin in coins:
+            series_data['y{}'.format(index)] = y_data[coin.unit_code]
+            series_data['name{}'.format(index)] = 'Circulating {}'.format(coin.name)
+            series_data['extra{}'.format(index)] = {
+                "tooltip": {
+                    "y_start": "There are ",
+                    "y_end": " circulating"
+                },
+                "date_format": "%d %b %Y %H:%M:%S %p"
+            }
+            index += 1
+
+        chart_data = {
+            'chart_type': "lineChart",
+            'name': 'Circulating Currency',
+            'series_data': series_data,
+            'extra': {
+                'x_is_date': True,
+                'color_category': 'category10',
             },
-            "date_format": "%d %b %Y %H:%M:%S %p"
+
         }
 
-        for coin in connection.tenant.coins.all():
-            info_objects = Info.objects.filter(
-                unit=coin.unit_code
-            ).order_by(
-                'time_added'
-            )
+        print(chart_data)
 
-            x_data = []
-            y_data = []
-            y_value = 0.0
-
-            day_count = 0
-            for info_object in info_objects:
-                day = int(
-                    time.mktime(
-                        datetime.datetime(
-                            info_object.time_added.year,
-                            info_object.time_added.month,
-                            info_object.time_added.day,
-                            0,
-                            0,
-                            0
-                        ).timetuple()
-                    ) * 1000
-                )
-
-                if len(x_data) == 0:
-                    x_data.append(day)
-
-                if day not in x_data:
-                    y_data.append(y_value / day_count)
-                    x_data.append(day)
-                    y_value = 0.0
-                    day_count = 0
-
-                y_value += float(info_object.money_supply)
-                day_count += 1
-
-            y_data.append(y_value / day_count)
-
-            coin_data['coins'].append(
-                {
-                    'type': "lineChart",
-                    'name': 'Circulating {}'.format(coin.name),
-                    'extra': {
-                        'x_is_date': True,
-                    },
-                    'container': '{}_container'.format(coin.unit_code),
-                    'data': {
-                        'x': x_data,
-                        'y1': y_data,
-                        'name1': 'Circulating {}'.format(coin.name),
-                        'extra1': extra_data,
-                        'color_category': 'category10'
-                    }
-                }
-            )
-
-        return render(request, 'charts/circulating_currency.html', coin_data)
+        return render(request, 'charts/circulating_currency.html', chart_data)
 
