0af22f05ae41b89b7558d34a736524bb257724a4
sammoth <sam.griffiths@wazoku.com>
2017-07-12 00:46:31 +0100

    update template tags for singular and plural

--- charts/templatetags/chart_extras.py
+++ charts/templatetags/chart_extras.py
@@ -8,6 +8,12 @@ def human_time(num_minutes):
     print(num_minutes)
     num_minutes = int(num_minutes)
 
+    if num_minutes < 60:
+        # less than 1 hour so return minutes
+        value = num_minutes
+        unit = 'minutes' if value == 1 else 'minutes'
+        return '{} {}'.format(value, unit)
+
     if num_minutes < 1440:
         # less than 1 day
         # return number of hours
@@ -15,8 +21,7 @@ def human_time(num_minutes):
         unit = 'hour' if value == 1 else 'hours'
         return '{} {}'.format(value, unit)
 
-    else:
-        # return number of days
-        value = int((num_minutes / 60) / 24)
-        unit = 'day' if value == 1 else 'days'
-        return '{} {}'.format(value, unit)
+    # return number of days
+    value = int((num_minutes / 60) / 24)
+    unit = 'day' if value == 1 else 'days'
+    return '{} {}'.format(value, unit)
