06ea73b076ea1c9a043a8c8629b1c5aee3946614
sammoth <sam.griffiths@wazoku.com>
2017-07-19 08:22:25 +0100

    remove Alerts form admin

--- alerts/admin.py
+++ alerts/admin.py
@@ -2,9 +2,3 @@ from django.contrib import admin
 
 from alerts.models import Alert
 
-
-class AlertAdmin(admin.ModelAdmin):
-    list_display = ('content_type', 'alert_operator', 'alert_value')
-    raw_id_fields = ('content_type',)
-
-admin.site.register(Alert, AlertAdmin)
