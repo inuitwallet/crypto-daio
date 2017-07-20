12c158883912bce8a5275d193de7add1bf9e6333
sammoth <sam.griffiths@wazoku.com>
2017-07-20 11:33:07 +0100

    update logging

--- blocks/models/block.py
+++ blocks/models/block.py
@@ -128,12 +128,15 @@ class Block(models.Model):
                 for rel_block in Block.objects.filter(next_block=block):
                     rel_block.next_block = None
                     rel_block.save(validate=False)
+                    logger.error('removed {} from {} next'.format(block, rel_block))
 
                 for rel_block in Block.objects.filter(previous_block=self):
                     rel_block.previous_block = None
                     rel_block.save(validate=False)
+                    logger.error('removed {} from {} previous'.format(block, rel_block))
 
                 block.delete()
+                logger.error('deleted {}'.format(block))
 
             self.next_block = None
             self.previous_block = None
