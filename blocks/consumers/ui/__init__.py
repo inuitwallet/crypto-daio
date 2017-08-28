fdab78f0b3f839c37cb8f300b8e44b0968ecf86a
sammoth <sam.griffiths@wazoku.com>
2017-08-28 20:00:44 +0100

    add address balance method

--- blocks/consumers/ui/__init__.py
+++ blocks/consumers/ui/__init__.py
@@ -1,7 +1,8 @@
-from .addresses import get_address_details
+from .addresses import get_address_details, get_address_balance
 from .blocks import get_block_transactions
 
 __all__ = [
     'get_address_details',
+    'get_address_balance',
     'get_block_transactions',
 ]
