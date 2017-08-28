531f5648001e661c53f84b2f4c1b8170bc4f05af
sammoth <sam.griffiths@wazoku.com>
2017-08-28 21:43:15 +0100

    add Orphan registry when blocks are invalid

--- blocks/models/__init__.py
+++ blocks/models/__init__.py
@@ -1,8 +1,8 @@
 from blocks.models.transaction import Transaction, TxInput, TxOutput
 from blocks.models.address import Address, WatchAddress
 from blocks.models.votes import CustodianVote, MotionVote, ParkRateVote, FeesVote, ParkRate
-from blocks.models.block import Block
 from blocks.models.network import Info, Peer, Orphan
+from blocks.models.block import Block
 
 __all__ = [
     'Block',
