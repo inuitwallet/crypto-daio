869bb599140ce63741e72eae32c68f7c7e821790
sammoth <sam.griffiths@wazoku.com>
2017-09-09 23:18:47 +0100

    losses script

--- blocks/management/commands/investigate_losses.py
+++ blocks/management/commands/investigate_losses.py
@@ -49,161 +49,13 @@ TARGET_ADDRESSES = [
     'ScDYXcJc4TShVLcKBmgRq9rz6ZvqfLrAkv',
 ]
 
+nodes = []
+edges = []
+
 
 class Command(BaseCommand):
     @staticmethod
-    def gather_tx_data():
-        with open('losses.csv', 'w+') as losses_file:
-            loss_writer = csv.writer(losses_file)
-            loss_writer.writerow(['Date', 'Block', 'From', 'To', 'Input', 'Outputs'])
-
-            for address in COMPROMISED_ADDRESSES:
-                logger.info('looking for {} data'.format(address))
-                try:
-                    addr = Address.objects.get(address=address)
-                except Address.DoesNotExist:
-                    logger.error('{} does not exist'.format(address))
-                    continue
-
-                matched_txs = []
-                for output in addr.outputs.all():
-                    try:
-                        if output.input.transaction in matched_txs:
-                            continue
-
-                        matched_txs.append(output.input.transaction)
-                        in_values = output.input.transaction.address_inputs
-                        out_values = output.input.transaction.address_outputs
-
-                        for value_address in out_values:
-                            if value_address in TARGET_ADDRESSES:
-                                logger.info(
-                                    '{} moved {} from {} to {} on {}'.format(
-                                        output.input.transaction,
-                                        out_values[value_address],
-                                        address,
-                                        value_address,
-                                        output.input.transaction.time
-                                    )
-                                )
-                                loss_writer.writerow(
-                                    [
-                                        output.input.transaction.time,
-                                        output.input.transaction.block.height,
-                                        address,
-                                        value_address,
-                                        in_values[address],
-                                        out_values[value_address]
-                                    ]
-                                )
-                    except TxInput.DoesNotExist:
-                        continue
-
-    @staticmethod
-    def get_balances():
-        with open('balances.csv', 'w+') as balances_csv:
-            balance_writer = csv.writer(balances_csv)
-            balance_writer.writerow(['Address', 'Balance'])
-            for address in COMPROMISED_ADDRESSES + TARGET_ADDRESSES:
-                logger.info('getting balance for {}'.format(address))
-                try:
-                    addr = Address.objects.get(address=address)
-                except Address.DoesNotExist:
-                    logger.error('{} does not exist'.format(address))
-                    continue
-                balance_writer.writerow([addr, addr.balance])
-
-    @staticmethod
-    def get_bad_txs():
-        with open('bad_txs.csv', 'w+') as bad_csv:
-            bad_writer = csv.writer(bad_csv)
-            bad_writer.writerow(['Date', 'Block', 'From', 'To', 'Input', 'Outputs'])
-
-            for address in TARGET_ADDRESSES:
-                logger.info('looking for {} data'.format(address))
-                try:
-                    addr = Address.objects.get(address=address)
-                except Address.DoesNotExist:
-                    logger.error('{} does not exist'.format(address))
-                    continue
-
-                matched_txs = []
-                for output in addr.outputs.all():
-                    try:
-                        if output.input.transaction in matched_txs:
-                            continue
-
-                        matched_txs.append(output.input.transaction)
-                        in_values = output.input.transaction.address_inputs
-                        out_values = output.input.transaction.address_outputs
-
-                        for value_address in out_values:
-                            logger.info(
-                                '{} moved {} from {} to {} on {}'.format(
-                                    output.input.transaction,
-                                    out_values[value_address],
-                                    address,
-                                    value_address,
-                                    output.input.transaction.time
-                                )
-                            )
-                            bad_writer.writerow(
-                                [
-                                    output.input.transaction.time,
-                                    output.input.transaction.block.height,
-                                    address,
-                                    value_address,
-                                    in_values[address],
-                                    out_values[value_address]
-                                ]
-                            )
-                    except TxInput.DoesNotExist:
-                        continue
-
-    def dedupe(self):
-        bad_addresses = [
-            'SYguMZJM1Y3wriyU2UWzXJg25UAxBQp65y',
-            'SURDRFLs8GoT16pVKcNT6igBexYqd19E6v',
-            'STE9MpqdhNm7Ln9LKQQuuUFtxGhR3RvwbH',
-            'SV3Jpc2yZzXVrwvy3ZSFR5FN6JEmYibQeF',
-            'STyfWicxBZPu2Kn1KaoCA81e4ddYPFUPxH',
-            'SN2722qP5zXbGeKrT236FtK1QsHxXBgqsH',
-            'SjuvNAUH1rAoicvKZ5Tgfo2e3HAxeK42iV',
-            'SdTSNATLyXZLHTbNr3L38GPfVsQZEhvxo3',
-            'SkTtUJWF7G7FRSPVKbh1ATNQV3r64S7c7U',
-            'SMUVkzMqYtabi2P3abFTegGZvLaUvxKRGs',
-            'SgoghD15S8sKFNo13ZgHWZPVcWpeki3qLa',
-            'SeVUrxMsX8N4pi79YjjEumenfPXv7FMNTw',
-            'SXpG2jQsZYiczy83K6jctBBKDgZZZzDe5H',
-            'ShnM5UZjSXqyBXurKXDvTL5xfbK8zBELt2',
-            'ShUQd6JvN2aPY3Yv8dC593NxpT3HKDKnQr',
-            'SQxrW62Sv1DZrL68kNf18u6D5H8ezq6mRN',
-            'SbkCReYV5EPWxjt5PiKC5dsvEbwruvK8MC',
-            'SN8qXUbj6bkZgHHktw5RtWR6YPtY6Vgdej',
-            'SXfkCbPDgTJW1EAdA2WMX28N3VXTRy6j3H',
-            'SfPuW3x4C2BZzUP6piWj4Y1hy9Qwn8uPSA',
-            'Shm9y9t8mYbcP9yXTKGFFTfMDEepPQKDhs',
-            'SirunbyyuxLGSPy2d8QNTUBv4Li7JLSC9v',
-            'SUTt3nHAymXEuH6svTbvYmQT1cBEpsikwK',
-            'SX6MMprHZgYd9J8euZrJMEJJFLanLQ89HE',
-            'SNRVfoGSsArzC1wN81xTxn5HnCKz5xNHmG',
-            'SjQSKz9twZoLZxEgMQ5ZA8GMFfG4mgD6gq'
-        ]
-        print([address for address in bad_addresses if address in COMPROMISED_ADDRESSES])
-
-    def get_spent_outputs(self, address):
-        spent = []
-        for output in address.outputs.all():
-            try:
-                if output.input:
-                    if output.input.transaction.index == 1:
-                        continue
-                    spent.append(output.input)
-            except TxInput.DoesNotExist:
-                pass
-        return spent
-
-    def get_transactions(self, address):
+    def get_transactions(address):
         txs = Transaction.objects.filter(
             input__previous_output__address=address
         ).exclude(
@@ -222,6 +74,48 @@ class Command(BaseCommand):
 
         return distinct
 
+    def handle_tx(self, tx):
+        # add the Tx to the nodes
+        if not any(node['id'] == tx.tx_id[:6] for node in nodes):
+            nodes.append({'id': tx.tx_id[:6], 'label': tx.tx_id})
+
+        address_inputs = tx.address_inputs
+        for input_address in address_inputs:
+            # for each input add an edge from the address to the tx.
+            # Add the address if it doesn't exist
+            if not any(node['id'] == input_address for node in nodes):  # noqa
+                nodes.append({
+                    'id': input_address,
+                    'label': input_address
+                })
+            edges.append({
+                'from': input_address,
+                'to': tx.tx_id[:6],
+                'value': address_inputs.get(input_address, 0) / 100000000
+            })
+
+        address_outputs = tx.address_outputs
+        for output_address in address_outputs:
+            # for each output add an edge from the address to the tx.
+            # Add the address if it doesn't exist
+            if not any(node['id'] == output_address for node in nodes):  # noqa
+                nodes.append({
+                    'id': output_address,
+                    'label': output_address
+                })
+            edges.append({
+                'from': tx.tx_id[:6],
+                'to': output_address,
+                'value': address_outputs.get(output_address, 0) / 100000000
+            })
+
+        for tx_output in tx.outputs.all():
+            try:
+                if tx_output.input:
+                    self.handle_tx(tx_output.input.transaction)
+            except TxInput.DoesNotExist:
+                pass
+
     def handle(self, *args, **options):
         """
         investigate the losses by tracking activity through the blockchain
@@ -229,9 +123,6 @@ class Command(BaseCommand):
         :param options:
         :return:
         """
-        nodes = []
-        edges = []
-
         for address in COMPROMISED_ADDRESSES:
             logger.info('working on {}'.format(address))
             # add the address to the nodes
@@ -240,60 +131,7 @@ class Command(BaseCommand):
             txs = self.get_transactions(a)
 
             for tx in txs:
-                # add the Tx to the nodes
-                if not any(node['id'] == tx.tx_id for node in nodes):
-                    nodes.append({'id': tx.tx_id, 'label': tx.tx_id})
+                self.handle_tx(tx)
 
-                address_inputs = tx.address_inputs
-                for input_address in address_inputs:
-                    # for each input add an edge from the address to the tx.
-                    # Add the address if it doesn't exist
-                    if not any(node['id'] == input_address for node in nodes):  # noqa
-                        nodes.append({
-                            'id': input_address,
-                            'label': input_address
-                        })
-                    edges.append({
-                        'from': input_address,
-                        'to': tx.tx_id,
-                        'value': address_inputs.get(input_address, 0)/100000000
-                    })
-
-                address_outputs = tx.address_outputs
-                for output_address in address_outputs:
-                    # for each output add an edge from the address to the tx.
-                    # Add the address if it doesn't exist
-                    if not any(node['id'] == output_address for node in nodes):  # noqa
-                        nodes.append({
-                            'id': output_address,
-                            'label': output_address
-                        })
-                    edges.append({
-                        'from': tx.tx_id,
-                        'to': output_address,
-                        'value': address_outputs.get(output_address, 0) / 100000000
-                    })
-
-                    # try:
-                    #     if tx_output.input:
-                    #         pass
-                    #         # calculate again for spending transaction
-                    # except TxInput.DoesNotExist:
-                    #     # unspent so add the address as a node
-                    #     # if necessary and add an edge
-                    #     if not any(node['id'] == tx_output.address.address for node in nodes):  # noqa
-                    #         nodes.append({
-                    #             'id': tx_output.address.address,
-                    #             'label': tx_output.address.address
-                    #         })
-                    #     edges.append({
-                    #         'from': tx_output.transaction.tx_id,
-                    #         'to': tx_output.address.address,
-                    #         'value': tx_output.value/100000000
-                    #     })
-            json.dump(nodes, open('nodes.json', 'w+'))
-            json.dump(edges, open('edges.json', 'w+'))
-            print('yup')
-
-        print(nodes)
-        print(edges)
+        json.dump(nodes, open('nodes.json', 'w+'))
+        json.dump(edges, open('edges.json', 'w+'))
