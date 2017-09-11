8eb84dd33f369a10884dcfaabb88574a896d8f94
sammoth <sam.griffiths@wazoku.com>
2017-09-11 11:44:09 +0100

    losses script

--- blocks/management/commands/investigate_losses.py
+++ blocks/management/commands/investigate_losses.py
@@ -15,18 +15,18 @@ tz = timezone.get_current_timezone()
 
 COMPROMISED_ADDRESSES = [
     'SSajkovCPXwdw46nyJ7vpTDkwtRZJzyY2z',
-    # 'SNf4uyshit1fj8dWKVxHsKTgTrNR61RskY',
-    # 'SQTHenWRCF7tZQb5RQAbf3pVYN3Jq5RET4',
-    # 'ShGVUEJpyZBTgK6V5ZzBorv899R1LP7pqm',
-    # 'SNdbH9sUJ8z33iE8oNBCwCLfwP9tafyZh3',
-    # 'Sb84GHDPxy1dzE4VttDTrLwYLzLw4hEDUV',
-    # 'SUgGG6PYXeoXtrUU85rViuWbxsVczwQX7i',
-    # 'SRcyHX5JE1tprmtUNswHFsgWqwciwkqigk',
-    # 'SMv2C8x41mtkZvv5wNejdqSsBQPPTfPEDj',
-    # 'SQGuknAk53MpBMy9fuX632Kqi8FWoNMQ2v',
-    # 'SYrndApJNq5JrXGu83NQuzb3PHQoaeEwx8',
-    # 'SXQcdc5THvdUAdfmK4NEYQpvqANwz4iBHg',
-    # 'SeTPb7fj6PLn2E4aMa5TbR83Pw6MSs37fM',
+    'SNf4uyshit1fj8dWKVxHsKTgTrNR61RskY',
+    'SQTHenWRCF7tZQb5RQAbf3pVYN3Jq5RET4',
+    'ShGVUEJpyZBTgK6V5ZzBorv899R1LP7pqm',
+    'SNdbH9sUJ8z33iE8oNBCwCLfwP9tafyZh3',
+    'Sb84GHDPxy1dzE4VttDTrLwYLzLw4hEDUV',
+    'SUgGG6PYXeoXtrUU85rViuWbxsVczwQX7i',
+    'SRcyHX5JE1tprmtUNswHFsgWqwciwkqigk',
+    'SMv2C8x41mtkZvv5wNejdqSsBQPPTfPEDj',
+    'SQGuknAk53MpBMy9fuX632Kqi8FWoNMQ2v',
+    'SYrndApJNq5JrXGu83NQuzb3PHQoaeEwx8',
+    'SXQcdc5THvdUAdfmK4NEYQpvqANwz4iBHg',
+    'SeTPb7fj6PLn2E4aMa5TbR83Pw6MSs37fM',
 ]
 
 TARGET_ADDRESSES = [
@@ -116,7 +116,7 @@ class Command(BaseCommand):
                 'to': tx.tx_id[:6],
                 'value': address_inputs.get(input_address, 0) / 100000000,
                 'title': address_inputs.get(input_address, 0) / 100000000,
-                'color': 'grey',
+                'color': '#fceb6a',
                 'arrows': 'middle'
             })
 
@@ -146,7 +146,7 @@ class Command(BaseCommand):
                 'to': address,
                 'value': output_totals['unspent'].get(address, 0) / 100000000,
                 'title': output_totals['unspent'].get(address, 0) / 100000000,
-                'color': 'grey',
+                'color': '#f4a84b',
                 'arrows': 'middle'
             })
 
