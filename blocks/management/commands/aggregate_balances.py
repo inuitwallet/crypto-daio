470cde0512e5dab33fdbe19988f4d3be775f513c
sammoth <sam.griffiths@wazoku.com>
2017-10-11 20:55:54 +0100

    calculate aggregate balances for quarantined addresses

--- blocks/management/commands/aggregate_balances.py
+++ blocks/management/commands/aggregate_balances.py
@@ -5,26 +5,26 @@ from django.core.management import BaseCommand
 from blocks.models import Address
 
 blocked_addresses = [
-    'SYAxC194NgWoAqtGHHZ4GKXU6LT7mgqAq1'
-    'Sk2P6oj9VCC8VmCBQaRWK4iwTPbvPcGPsh'
-    'SQoAn2DmRPv2ypLcLR1NT3ciqfh8X9qbDQ'
-    'SWvFUTiJ2gvmUEGEszvXrdjkn6Uh9ZLbZe'
-    'SigAuqtYtigWZ8uVFG7iuLDYv9au9cK49L'
-    'SkNDumZBBbx5ygSRd7Nvbg32RoeUERoHrH'
-    'Sif3nJuoCZmG5zeahqAYy9jetRLAKfDawC'
-    'SYhzKRTPVR6PrqcHqZE1c2d7fWDQLqieba'
-    'SNi3zogpVvEGNLyxh6RLNHGU3PMPJBzgEY'
-    'SWxjKJ4m3U1X7h22yVDyDuHcdc5hwqxG6g'
-    'SYBAJJpoxwUBwyrLMud8B6FLibvZzbTCpi'
-    'Sbtxw5pC3y3QpepsWH5iAcra5a53ne6se9'
-    'SYxdao1Pyn92trMniivmw3UVLJ9wXbX6kz'
-    'SWVRAjpqjW4orkYmADPvQ1adUni9jVK7H8'
-    'SUJH1DLG2iLKHY3vXw4V5J1iLphCYAxg6n'
-    'ShQ4Rezxs3NKJHcRV6SVL1bvduT2n9P4bi'
-    'SewzCfwc36oFTXH9jL1VTUHsEPrJpnB2x2'
-    'SYy3bPPGqf3NipGyVRcgcu6rq7RKHoyh5Y'
-    'SQ7JRP54iQeLCHR8tujDJsAndKeiuyAqbi'
-    'BJDirPFfohpTRXTiTzw8gfZjV9qy3CRqpM'
+    'SYAxC194NgWoAqtGHHZ4GKXU6LT7mgqAq1',
+    'Sk2P6oj9VCC8VmCBQaRWK4iwTPbvPcGPsh',
+    'SQoAn2DmRPv2ypLcLR1NT3ciqfh8X9qbDQ',
+    'SWvFUTiJ2gvmUEGEszvXrdjkn6Uh9ZLbZe',
+    'SigAuqtYtigWZ8uVFG7iuLDYv9au9cK49L',
+    'SkNDumZBBbx5ygSRd7Nvbg32RoeUERoHrH',
+    'Sif3nJuoCZmG5zeahqAYy9jetRLAKfDawC',
+    'SYhzKRTPVR6PrqcHqZE1c2d7fWDQLqieba',
+    'SNi3zogpVvEGNLyxh6RLNHGU3PMPJBzgEY',
+    'SWxjKJ4m3U1X7h22yVDyDuHcdc5hwqxG6g',
+    'SYBAJJpoxwUBwyrLMud8B6FLibvZzbTCpi',
+    'Sbtxw5pC3y3QpepsWH5iAcra5a53ne6se9',
+    'SYxdao1Pyn92trMniivmw3UVLJ9wXbX6kz',
+    'SWVRAjpqjW4orkYmADPvQ1adUni9jVK7H8',
+    'SUJH1DLG2iLKHY3vXw4V5J1iLphCYAxg6n',
+    'ShQ4Rezxs3NKJHcRV6SVL1bvduT2n9P4bi',
+    'SewzCfwc36oFTXH9jL1VTUHsEPrJpnB2x2',
+    'SYy3bPPGqf3NipGyVRcgcu6rq7RKHoyh5Y',
+    'SQ7JRP54iQeLCHR8tujDJsAndKeiuyAqbi',
+    'BJDirPFfohpTRXTiTzw8gfZjV9qy3CRqpM',
 ]
 
 logger = logging.getLogger(__name__)
