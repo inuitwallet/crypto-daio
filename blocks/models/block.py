2f1bc50232eedafb5d755e67ed9a51cd2d35bf5c
inuitwallet <contact@inuit-wallet.co.uk>
2017-12-29 23:07:34 +0000

    use float formatting

--- blocks/models/block.py
+++ blocks/models/block.py
@@ -395,7 +395,7 @@ class Block(CachingMixin, models.Model):
                     logger.info(Decimal(format(rate.get('rate', 0), '.10f')))
                     park_rate, _ = ParkRate.objects.get_or_create(
                         blocks=rate.get('blocks', 0),
-                        rate=Decimal(format(rate.get('rate', 0), '.10f')
+                        rate=Decimal(format(rate.get('rate', 0), '.10f'))
                     )
                 except ParkRate.MultipleObjectsReturned:
                     logger.error(
