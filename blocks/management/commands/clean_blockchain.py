e8fcceba16cca8dc6edeaad2085da6578a6909ac
inuitwallet <contact@inuit-wallet.co.uk>
2018-03-30 14:24:16 +0100

    add management command to remove orphaned blocks and transactions

---
+++ blocks/management/commands/clean_blockchain.py
@@ -0,0 +1,18 @@
+import logging
+
+from django.core.management import BaseCommand
+
+from blocks.models import Block, Transaction
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        # delete blocks with no height
+        deleted_blocks = Block.objects.filter(height__isnull=True).delete()
+        logger.info('Removed Blocks: {}'.format(deleted_blocks))
+
+        # delete transactions with no block
+        deleted_transactions = Transaction.objects.filter(block__isnull=True).delete()
+        logger.info('Removed Txs: {}'.format(deleted_transactions))
