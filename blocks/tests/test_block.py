0ecc59d290b74a4ac2bf76aabc7a45933cd2a817
MJ2P <mj2p.sg@gmail.com>
2020-09-22 23:28:08 +0100

    add serialize test and up fail coverage to 20

--- blocks/tests/test_block.py
+++ blocks/tests/test_block.py
@@ -1,8 +1,13 @@
 import hashlib
+import json
+import string
+from datetime import datetime
+from random import randint, choice, uniform
 
+from django.utils.timezone import make_aware
 from tenant_schemas.test.cases import TenantTestCase
 
-from blocks.models import Block
+from blocks.models import Block, Transaction, TxOutput, Address
 
 
 class TestBlock(TenantTestCase):
@@ -74,3 +79,91 @@ class TestBlock(TenantTestCase):
         altered = new_block.validate_block_height()
         # we want 'altered' to be False to show that no alteration of the block took place
         self.assertFalse(altered)
+
+    def test_serialize(self):
+        # create block parameters
+        height = randint(1000, 2000)
+        hash = hashlib.sha256(b"Serialize Block").hexdigest()
+        size = randint(1, 5000)
+        version = randint(1, 1000)
+        merkleroot = hashlib.sha256(b"Merkle").hexdigest()
+        time = make_aware(datetime(randint(2014, 2020), randint(1, 12), randint(1, 28)))
+        nonce = randint(1, 4)
+        bits = "".join(choice(string.hexdigits) for _ in range(10))
+        difficulty = uniform(1, 500)
+        mint = uniform(1, 500)
+        previous_block = Block.objects.create(
+            height=height - 1, hash=hashlib.sha256(b"Previous Block").hexdigest()
+        )
+        next_block = Block.objects.create(
+            height=height + 1, hash=hashlib.sha256(b"Next Block").hexdigest()
+        )
+        flags = "".join(choice(string.hexdigits) for _ in range(randint(100, 610)))
+        proof_hash = "".join(choice(string.hexdigits) for _ in range(randint(100, 610)))
+        entropy_bit = randint(1, 1000)
+        modifier = "".join(choice(string.hexdigits) for _ in range(randint(100, 610)))
+        modifier_checksum = "".join(
+            choice(string.hexdigits) for _ in range(randint(100, 610))
+        )
+        coinage_destroyed = randint(1, 5000)
+
+        # create the block
+        block = Block.objects.create(
+            height=height,
+            hash=hash,
+            size=size,
+            version=version,
+            merkle_root=merkleroot,
+            time=time,
+            nonce=nonce,
+            bits=bits,
+            difficulty=difficulty,
+            mint=mint,
+            flags=flags,
+            proof_hash=proof_hash,
+            entropy_bit=entropy_bit,
+            modifier=modifier,
+            modifier_checksum=modifier_checksum,
+            coinage_destroyed=coinage_destroyed,
+            previous_block=previous_block,
+            next_block=next_block,
+        )
+        # create two basic transaction
+        Transaction.objects.create(
+            tx_id=hashlib.sha256(b"Tx0").hexdigest(), block=block, index=0
+        )
+        tx1 = Transaction.objects.create(
+            tx_id=hashlib.sha256(b"Tx1").hexdigest(), block=block, index=1
+        )
+        # create an output for tx 1 at index 1 with an address output
+        address = "".join(choice(string.hexdigits) for _ in range(28))
+        TxOutput.objects.create(
+            transaction=tx1, index=1, address=Address.objects.create(address=address)
+        )
+
+        serialized_block = block.serialize()
+        expected = {
+            "hash": hash,
+            "height": height,
+            "size": size,
+            "version": version,
+            "merkleroot": merkleroot,
+            "time": datetime.strftime(time, "%Y-%m-%d %H:%M:%S %Z"),
+            "nonce": nonce,
+            "bits": bits,
+            "difficulty": difficulty,
+            "mint": mint,
+            "flags": flags,
+            "proofhash": proof_hash,
+            "entropybit": entropy_bit,
+            "modifier": modifier,
+            "modifierchecksum": modifier_checksum,
+            "coinagedestroyed": coinage_destroyed,
+            "previousblockhash": previous_block.hash,
+            "nextblockhash": next_block.hash,
+            "valid": False,
+            "number_of_transactions": 2,
+            "solved_by": address,
+        }
+
+        self.assertEqual(serialized_block, expected)
